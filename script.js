33e361fc8925e38f0853d81d15ba026cfffe4c78
GoKul-GoKul <85825587+GoKul-GoKul@users.noreply.github.com>
2023-12-28 20:52:04 +0530

    First Commit
    Almost finished the project, but minor updates to be done

---
+++ script.js
@@ -0,0 +1,65 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+let score = 20
+let highScore = 0;
+
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+}
+
+function checkGuessing() {
+
+    const guess = Number(document.querySelector('.guess').value);
+
+    if (!guess) {
+        displayMessage('⛔ No number!');
+    }
+
+    else if (guess === secretNumber) {
+
+        displayMessage('🎉 Correct Number');
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('.number').style.width = '30rem';
+
+        document.body.style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+
+    }
+
+    else if (guess !== secretNumber) {
+        if (score > 1) {
+            displayMessage(guess > secretNumber ? `${guess} is 📈Too high!` : `${guess} is 📉Too low!`);
+            score--;
+            document.querySelector('.score').textContent = score;
+            document.querySelector('.guess').value = '';
+        } else {
+            displayMessage('💥You lost the game');
+        }
+    }
+
+}
+
+function againButton() {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    console.log(secretNumber);
+
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.score').textContent = score;
+    displayMessage('Start guessing...');
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+
+}
+
+document.querySelector('.check').addEventListener('click', checkGuessing());
+
+document.querySelector('.again').addEventListener('click', againButton());
